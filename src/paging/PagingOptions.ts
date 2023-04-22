3ec3a163f923f18e95f5513b79f5fda973550e00
Rafał Majewski <goodheropl@gmail.com>
2023-04-22 22:57:11 +0200

    Sync with template

--- src/paging/PagingOptions.ts
+++ src/paging/PagingOptions.ts
@@ -1,7 +1,19 @@
-class PagingOptions {
-	public readonly skip: number = 0;
+import {IsInt, IsOptional, Max, Min} from "class-validator";
+import {Type} from "class-transformer";
 
-	public readonly take: number = 10;
+class PagingOptions {
+	@IsInt()
+	@Min(0)
+	@Type(() => Number)
+	@IsOptional()
+	public readonly skip = 0;
+
+	@IsInt()
+	@Min(0)
+	@Max(100)
+	@Type(() => Number)
+	@IsOptional()
+	public readonly take = 10;
 }
 
 export default PagingOptions;
